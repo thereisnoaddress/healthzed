f649e6bf0f872ff7c7e3746c2ee4b309a83d7e02
Chris Zhu <thereisnoaddress@gmail.com>
2023-07-05 11:19:47 -0700

    add stuff to make it work now

--- endpoint.py
+++ endpoint.py
@@ -5,6 +5,10 @@ from protocol import PingRequest, PingResponse
 
 app = FastAPI()
 
+@app.get("/healthz"):
+def healthzee():
+    return True
+
 
 @app.post("/send_ping")
 def send_ping(from_user, to_user):
