1ff79339cb0d25c9bdd4d7dbf695af079fde4a32
Chris Zhu <thereisnoaddress@gmail.com>
2023-07-05 11:19:09 -0700

    add stuff to make it work now

--- endpoint.py
+++ endpoint.py
@@ -7,10 +7,9 @@ app = FastAPI()
 
 
 @app.post("/send_ping")
-def send_ping(PingRequest):
-    from_user = PingRequest.from_user
-    to_user = PingRequest.to_user
-
-    deliver_ping.delay(from_user, to_user)
-
-    return PingResponse(status_code=200, message="Ping enqueued successfully!")
+def send_ping(from_user, to_user):
+    # TODO: add celery queue config
+    # deliver_ping.delay(from_user, to_user)
+    response = deliver_ping(from_user=from_user, to_user=to_user)
+    if response:
+        return PingResponse(status_code=200, message="Ping enqueued successfully!")
--- main.py
+++ main.py
@@ -1,7 +1,14 @@
+import logging
+
+logger = logging.getLogger()
+
+
 def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
     return f"Hi, {name}"
 
 
 def deliver_ping(from_user, to_user):
-    pass
+    # TODO: add some sort of message delivery service? like AWS SNS
+    logger.info(f"delivering ping from {from_user} to {to_user}")
+    return True
