5a7b7c2ebd44385a888c4635ce21199321335040
Chris Zhu <thereisnoaddress@gmail.com>
2023-07-05 11:19:52 -0700

    Revert "add stuff to make it work now"
    This reverts commit f649e6bf0f872ff7c7e3746c2ee4b309a83d7e02.

--- endpoint.py
+++ endpoint.py
@@ -5,10 +5,6 @@ from protocol import PingRequest, PingResponse
 
 app = FastAPI()
 
-@app.get("/healthz"):
-def healthzee():
-    return True
-
 
 @app.post("/send_ping")
 def send_ping(from_user, to_user):
