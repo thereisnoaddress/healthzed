795accf806292e293efe7cfcd3b4bbbb5439e746
Chris Zhu <thereisnoaddress@gmail.com>
2023-08-30 12:12:53 +0800

    remove users for now since we're not using them

--- healthzed/main.py
+++ healthzed/main.py
@@ -6,17 +6,12 @@ logging.config.fileConfig("logging.conf")
 logger = logging.getLogger(__name__)
 
 from healthzed.notification_service import NotificationService
-from healthzed.protocol import HealthzedUser
 
 notification_service = NotificationService()
 
 
-def deliver_ping(
-    from_user: HealthzedUser, to_user: HealthzedUser, message: str, phone_number: str
-):
-    logger.info(
-        f"delivering ping from {from_user.id} to {to_user.id} to phone number {phone_number}..."
-    )
+def deliver_ping(message: str, phone_number: str):
+    logger.info(f"delivering ping to phone number {phone_number}...")
     notification_service.send_sns_notification(
         phone_number=phone_number, message=message
     )
