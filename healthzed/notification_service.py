32fd2583887399ce6c46c7b51232a8998124371a
Chris Zhu <thereisnoaddress@gmail.com>
2023-10-03 11:33:57 +0700

    add pinpoint support for both ways

--- healthzed/notification_service.py
+++ healthzed/notification_service.py
@@ -23,6 +23,12 @@ class NotificationService:
             aws_secret_access_key=os.environ["AWS_SECRET_KEY"],
             region_name=os.environ["AWS_REGION"],
         )
+        self.pinpoint_client = boto3.client(
+            "pinpoint",
+            aws_access_key_id=os.environ["AWS_ACCESS_KEY"],
+            aws_secret_access_key=os.environ["AWS_SECRET_KEY"],
+            region_name=os.environ["AWS_PINPOINT_REGION"],
+        )
 
     def send_sns_notification(self, phone_number, message="Test SNS message!"):
         try:
@@ -38,3 +44,33 @@ class NotificationService:
             raise
         else:
             return message_id
+
+    def send_pinpoint_sms_notification(
+        self,
+        destination_number,
+        message,
+        message_type="PROMOTIONAL",
+        origination_number="+18078085477",
+        app_id="8bd98417d59a452f96fcc2b60cbd13cf",
+    ):
+        try:
+            response = self.pinpoint_client.send_messages(
+                ApplicationId=app_id,
+                MessageRequest={
+                    "Addresses": {destination_number: {"ChannelType": "SMS"}},
+                    "MessageConfiguration": {
+                        "SMSMessage": {
+                            "Body": message,
+                            "MessageType": message_type,
+                            "OriginationNumber": origination_number,
+                        }
+                    },
+                },
+            )
+        except ClientError:
+            logger.exception("Couldn't send message.")
+            raise
+        else:
+            return response["MessageResponse"]["Result"][destination_number][
+                "MessageId"
+            ]
